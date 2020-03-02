a12bba699ff7ec90d90d1bdd416cd47baaf2cf84
j4jithinmohan <j4jithinmohan@gmail.com>
2020-03-02 22:43:56 +0530

    reject requested based on a query parameter

--- src/main/java/com/springfilter/filters/TestFilter.java
+++ src/main/java/com/springfilter/filters/TestFilter.java
@@ -1,6 +1,9 @@
 package com.springfilter.filters;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,8 +11,11 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 
 public class TestFilter implements Filter {
+
+	private String parameter;
 
 	public void destroy() {
 		System.out.println("Destroy filter...");
@@ -18,11 +24,25 @@ public class TestFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		System.out.println("Do Filter...");
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setHeader("TR", "Test Response");
+
+		List<String> params = Collections.list(request.getParameterNames());
+
+		for (String param : params) {
+			if (this.parameter.equalsIgnoreCase(param)) {
+				res.sendError(HttpServletResponse.SC_FORBIDDEN,
+						String.format("Does not allow parameter %s", this.parameter));
+				return;
+			}
+		}
+
 		chain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
 		System.out.println("Init Filter....");
+		this.parameter = "test";
 	}
 
 }
